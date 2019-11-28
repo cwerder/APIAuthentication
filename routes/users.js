8749fe55fb7ef428df8cf2853b4b4b0e23363afe
CJ Werder <cwerder15@gmail.com>
2019-11-27 22:44:03 -0600

    Establish express http server and basic routing

---
+++ routes/users.js
@@ -0,0 +1,11 @@
+const router = require('express-promise-router')();
+
+const UsersController = require('../controllers/users');
+
+router.post('/signup', UsersController.signUp);
+
+router.post('/signin', UsersController.signIn);
+
+router.get('/secret', UsersController.secret);
+
+module.exports = router;
