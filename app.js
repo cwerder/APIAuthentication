c72ede478b56ac5f800c45cdf6e88cc85e2c713b
CJ Werder <cwerder15@gmail.com>
2019-11-28 22:19:48 -0600

    Establish server side validation and create mongodb instance locally

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/APIAuthentication', 
+{ useNewUrlParser: true, useUnifiedTopology: true });
 
 const app = express();
 
--- controllers/users.js
+++ controllers/users.js
@@ -1,11 +1,31 @@
+const User = require('../Models/user');
+
 module.exports = {
     signUp: async (req, res, next) => {
-        console.log('UsersController.signUp called')
+        console.log('UsersController.signUp called');
+        const { email, password } = req.body;
+
+        // Check if there is a user with the same email
+        const foundUser = await User.findOne({ email })
+
+        if (foundUser) {
+            return res.status(403)
+                .json({ error: 'Account is already created'});
+        }
+
+        // Create a new user
+        const newUser = new User({ email, password });
+
+        const createdUser = await newUser.save();
+
+        // Respond with token
+        res.json({ user: createdUser })
     },
     signIn: async (req, res, next) => {
-        console.log('UsersController.signIn called')
+        // Generate token
+        console.log('UsersController.signIn called');
     },
     secret: async (req, res, next) => {
-        console.log('UsersController.secret called')
+        console.log('UsersController.secret called');
     }
 }
